3b049aeb832309a0451f8286e374aef4d13b40fa
Elizabeth Engelman <emengelman@gmail.com>
2019-02-05 16:10:37 -0600

    Open a trie from the in-memory database

--- statediff/builder/builder.go
+++ statediff/builder/builder.go
@@ -28,6 +28,7 @@ import (
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/ethereum/go-ethereum/trie"
+	"github.com/ethereum/go-ethereum/core"
 )
 
 type Builder interface {
