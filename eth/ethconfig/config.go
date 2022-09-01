7ea71bca678ede56fc982e6b2b43899991a4782d
Michael <michael@abastionofsanity.com>
2022-09-01 14:34:58 -0400

    V1.10.25 statediff v4 wip (#275)
    * Statediff Geth
    
    Handle conflicts (#244)
    
    * Handle conflicts
    
    * Update go mod file versions
    
    * Make lint changes
    
    Disassociate block number from the indexer object
    
    Update ipld-eth-db ref
    
    Refactor builder code to make it reusable
    
    Use prefix comparison for account selective statediffing
    
    Update builder unit tests
    
    Add mode to write to CSV files in statediff file writer (#249)
    
    * Change file writing mode to csv files
    
    * Implement writer interface for file indexer
    
    * Implement option for csv or sql in file mode
    
    * Close files in CSV writer
    
    * Add tests for CSV file mode
    
    * Implement CSV file for watched addresses
    
    * Separate test configs for CSV and SQL
    
    * Refactor common code for file indexer tests
    
    Update indexer to include block hash in receipts and logs (#256)
    
    * Update indexer to include block hash in receipts and logs
    
    * Upgrade ipld-eth-db image in docker-compose to run tests
    
    Use watched addresses from direct indexing params by default while serving statediff APIs (#262)
    
    * Use watched addresses from direct indexing params in statediff APIs by default
    
    * Avoid using indexer object when direct indexing is off
    
    * Add nil check before accessing watched addresses from direct indexing params
    
    Rebase missed these changes needed at 1.10.20
    
    Flags cleanup for CLI changes and linter complaints
    
    Linter appeasements to achieve perfection
    
    enforce go 1.18 for check (#267)
    
    * enforce go 1.18 for check
    
    * tests on 1.18 as well
    
    * adding db yml for possible change in docker-compose behavior in yml parsing
    
    Add indexer tests for handling non canonical blocks (#254)
    
    * Add indexer tests for header and transactions in a non canonical block
    
    * Add indexer tests for receipts in a non-canonical block and refactor
    
    * Add indexer tests for logs in a non-canonical block
    
    * Add indexer tests for state and storage nodes in a non-canonical block
    
    * Add indexer tests for non-canonical block at another height
    
    * Avoid passing address of a pointer
    
    * Update refs in GitHub workflow
    
    * Add genesis file path to stack-orchestrator config in GitHub workflow
    
    * Add descriptive comments
    
    fix non-deterministic ordering in unit tests
    
    Refactor indexer tests to avoid duplicate code (#270)
    
    * Refactor indexer tests to avoid duplicate code
    
    * Refactor file mode indexer tests
    
    * Fix expected db stats for sqlx after tx closure
    
    * Refactor indexer tests for legacy block
    
    * Refactor mainnet indexer tests
    
    * Refactor tests for watched addressess methods
    
    * Fix query in legacy indexer test
    
    rebase and resolve onto 1.10.23... still error out of index related to GetLeafKeys
    
    changed trie.Commit behavior was subtle about not not flushing to disk without an Update
    
    * no merge nodeset throws nil
    
    * linter appeasement
    
    Co-authored-by: Abdul Rabbani <abdulrabbani00@gmail.com>

--- eth/ethconfig/config.go
+++ eth/ethconfig/config.go
@@ -214,6 +214,10 @@ type Config struct {
 
 	// OverrideTerminalTotalDifficultyPassed (TODO: remove after the fork)
 	OverrideTerminalTotalDifficultyPassed *bool `toml:",omitempty"`
+
+	// Signify whether or not we are producing statediffs
+	// If we are, do not dereference state roots until the statediffing service is done with them
+	Diffing bool
 }
 
 // CreateConsensusEngine creates a consensus engine for the given chain configuration.
