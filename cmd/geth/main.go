302d49d0c27103726118333154db3e1aabbe8ae2
Thomas E Lackey <telackey@bozemanpass.com>
2023-03-07 12:54:27 -0600

    Add flag

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -175,6 +175,7 @@ var (
 		utils.StateDiffWatchedAddressesFilePath,
 		utils.StateDiffUpsert,
 		utils.StateDiffLogStatements,
+		utils.StateDiffCopyFrom,
 		configFileFlag,
 	}, utils.NetworkFlags, utils.DatabasePathFlags)
 
