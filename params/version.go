b83e05b6046924cb432e36b9a549c19881348f82
i-norden <iansnorden@gmail.com>
2023-05-18 12:46:05 -0500

    bump meta version

--- params/version.go
+++ params/version.go
@@ -24,7 +24,7 @@ const (
 	VersionMajor = 1                       // Major version component of the current release
 	VersionMinor = 11                      // Minor version component of the current release
 	VersionPatch = 5                       // Patch version component of the current release
-	VersionMeta  = "statediff-5.0.4-alpha" // Version metadata to append to the version string
+	VersionMeta  = "statediff-5.0.5-alpha" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
